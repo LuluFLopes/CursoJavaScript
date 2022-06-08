ee43320b0eadd261aa4db8d93f5d9746049fa0e6
Luciano <luc.merce@hotmail.com>
2022-06-08 16:42:14 -0300

    Capturando erros de requisição

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -6,17 +6,30 @@ botaoAdicionar.addEventListener("click", function () {
 
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes"); // Abre a requisição com os parâmetros desejados.
 
+    // xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes111"); <= Requisição com erro de endereço para teste.
+
     xhr.addEventListener("load", function () { // Evento load para executar comandos após o carregamento da página.
 
-        var resposta = xhr.responseText;
+        if (xhr.status == 200) { // Garante que a requisição foi realizada com sucesso.
+            erroAjax.classList.add("invisivel");
+            erroAjax.classList.remove("erro-ajax"); 
 
-        var pacientes = JSON.parse(resposta);
+            var resposta = xhr.responseText; // Salva o arquivo json como string.
+            var pacientes = JSON.parse(resposta); // Converte o json em array.
 
-        pacientes.forEach(function (paciente) {
+            pacientes.forEach(function (paciente) {
 
-            adicionaPacienteNaTabela(paciente);
+                adicionaPacienteNaTabela(paciente);
 
-        });
+            });
+
+        } else {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            var erroAjax = document.querySelector("#erro-ajax");
+            erroAjax.classList.remove("invisivel");
+            erroAjax.classList.add("erro-ajax");
+        }
 
     });
 
